d824712e4798b0ddd7f72baae1d490b332546640
Abdel <abdelrahmanb015@gmail.com>
2021-12-22 23:23:35 +0200

    Removed type hints.

--- grid.py
+++ grid.py
@@ -1,23 +1,23 @@
 from random import randint
-from pygame import Surface, draw
+from pygame import draw
 from numpy import ndarray
 
 class Cell:
-    def __init__(self, color: tuple[int, int, int], isEmpty: bool):
+    def __init__(self, color, isEmpty):
         self.color = color
         self.isEmpty = isEmpty
 
-    def draw(self, surface: Surface, x: int, y: int, scale: int):
+    def draw(self, surface, x, y, scale):
         draw.rect(surface, color=self.color, rect=(x * scale,
                                                    y * scale,
                                                    scale,
                                                    scale))
 
 class HRoad(Cell):
-    def __init__(self, color: tuple[int, int, int]):
+    def __init__(self, color):
         super().__init__(color, isEmpty=True)
 
-    def draw(self, surface: Surface, x: int, y: int, scale: int):
+    def draw(self, surface, x, y, scale):
         width = 0.75 * scale
         x_shift = 0.125 * scale
         height = 0.25 * scale
@@ -28,10 +28,10 @@ class HRoad(Cell):
                                                    height))
 
 class VRoad(Cell):
-    def __init__(self, color: tuple[int, int, int]):
+    def __init__(self, color):
         super().__init__(color, isEmpty=True)
 
-    def draw(self, surface: Surface, x: int, y: int, scale: int):
+    def draw(self, surface, x, y, scale):
         width = 0.25 * scale
         x_shift = 0.375 * scale
         height = 0.75 * scale
@@ -42,18 +42,18 @@ class VRoad(Cell):
                                                    height))
 
 class Grid:
-    def __init__(self, rows: int, columns: int, element: Cell = None):
+    def __init__(self, rows, columns, element):
         self.rows = rows
         self.columns = columns
         self.cells = ndarray(shape=(rows, columns), dtype=Cell)
         self.cells.fill(element)
 
-    def fillWith(self, element: Cell, rows: tuple[int, int], columns: tuple[int, int]):
+    def fillWith(self, element, rows, columns):
         for x in range(rows[0], rows[1] + 1):
             for y in range(columns[0], columns[1] + 1):
                 self.cells[x][y] = element
 
-    def draw(self, surface: Surface, scale: int):
+    def draw(self, surface, scale):
         for y in range(self.rows):
             for x in range(self.columns):
                 self.cells[y][x].draw(surface, x, y, scale)
